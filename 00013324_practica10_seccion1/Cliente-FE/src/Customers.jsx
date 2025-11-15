9514025204b56fea40c3baacf9da81c945ede395
CarlosCruz99 <00013324@uca.edu.sv>
2025-11-14 21:48:12 -0600

    feat: Agregar visualización de customers en frontend

--- 00013324_practica10_seccion1/Cliente-FE/src/Customers.jsx
+++ 00013324_practica10_seccion1/Cliente-FE/src/Customers.jsx
@@ -12,16 +12,31 @@ const Customers = () => {
     customerList();
   }, []);
   
-
   return (
     <div>
       <h2>Customers</h2>
-
-      <ul>
-        {customers.map(c => (
-          <li key={c.id}>{c.name}</li>
-        ))}
-      </ul>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Address</th>
+              <th>Phone</th>
+              <th>Code</th>
+            </tr>
+          </thead>
+          <tbody>
+            {customers.map((c) => (
+              <tr key={c.id}>
+                <td>{c.id}</td>
+                <td>{c.name}</td>
+                <td>{c.address}</td>
+                <td>{c.phone}</td>
+                <td>{c.code}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
     </div>
   );
 };
